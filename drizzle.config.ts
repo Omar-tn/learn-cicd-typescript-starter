9ecb1ce7684ddb4b6d02c3123c224347b1655a1a
Omar-tn <omar.omar5750@gmail.com>
2026-04-05 12:00:59 +0300

    fix drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,13 @@ import {defineConfig} from "drizzle-kit";
 
 import {config} from "./src/config.js";
 
+import "dotenv/config";
+
 export default defineConfig({
     out: "./src/db/migrations",
     schema: "./src/db/schema.ts",
     dialect: "turso",
     dbCredentials: {
-        url: config.db.url || "",
+        url: process.env.DATABASE_URL || "", //config.db.url || "",
     },
 });
